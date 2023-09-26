b2205d5fc003ae258aa9e7f25772ffccf9622b2f
Teun van Schagen <t.vanschagen1996@gmail.com>
2023-09-26 21:15:13 +0200

    fix: limit perf tests to 3 kentekens due to runtime

--- perf/PerformanceTests.cs
+++ perf/PerformanceTests.cs
@@ -5,22 +5,7 @@ namespace Tvans.Kenteken.PerformanceTests;
 [MemoryDiagnoser]
 public class PerformanceTests
 {
-    [Params(
-        "99-XX-XX", 
-        "99-99-XX",
-        "99-XX-99",
-        "XX-99-XX",
-        "XX-XX-99",
-        "99-XX-XX",
-        "99-XXX-9",
-        "9-XXX-99",
-        "XX-999-X",
-        "X-999-XX",
-        "XXX-99-X",
-        "X-99-XXX",
-        "9-XX-999",
-        "999-XX-9"
-    )]
+    [Params("99-XX-XX", "99-99-XX", "99-XX-99")]
     public string Input { get; set; } = default!;
     
     [Benchmark]
